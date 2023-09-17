207b380d8157fe713496b37b420b8d3706133472
Syed Muhammad Zahir <s4630051@student.uq.edu.au>
2023-09-17 21:40:19 +1000

    Initiates repository structure and creates code for unet components to be used in the actual architecture of the model

---
+++ unet_model_components.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class UNetInConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.in_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+        
+    def forward(self, x):
+        return self.in_conv(x)
+    
+class UNetDownConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.down_conv = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2),
+            UNetInConv(in_channels, out_channels)
+        )
+        
+    def forward(self, x):
+        return self.down_conv(x)
+    
+class UNetUpConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up_conv = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2),
+            UNetInConv(in_channels, out_channels)
+        )
+        
+    def forward(self, x):
+        return self.up_conv(x)
+    
+class UNetOutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.out_conv = nn.Sequential(
+            UNetInConv(in_channels, out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1)
+        )
+        
+    def forward(self, x):
+        return self.out_conv(x)
